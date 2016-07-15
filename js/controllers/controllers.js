c86ecaae9836154ca74dd9a622322a6bb59606d8
Dominic Gagné <gagned@mail.uoguelph.ca>
2016-07-15 19:06:29 -0400

    Need to find a better email obfuscation strategy, spam bots are hell...

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -2,9 +2,5 @@ var BoilerPlate = angular.module('BoilerPlate.controllers',[])
 
 .controller('HomeCtrl',function($scope, $http, $route, $location, $mdDialog, $mdMedia) {
 
-    var username = "gagned";
-    var hostname = "mail.uoguelph.ca";
-    var linktext = username + "@" + hostname ;
-    //document.write("<a href='" + "mail" + "to:" + username + "@" + hostname + "'>" + linktext + "</a>");
 });
 
